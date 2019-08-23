920a0b8dacea3fec71425928e34294e38a3c7fd8
imshanmusanjay <v-shansa@microsoft.com>
2019-08-23 15:57:00 +0530

    [session 2 error handling] added try-catch

--- routes/restaurant.js
+++ routes/restaurant.js
@@ -26,19 +26,24 @@ module.exports = function ({ app, Restaurants, utils: { handleRating } }) {
 	});
 
 	app.post("/api/restaurant/rating", async (req, res) => {
-		//Query: keyword
-		const { name, value } = req.body;
-		if (name && value) {
+		try {
+			//Query: keyword
+			const { name, value } = req.body;
+			if (name && value) {
 
-			Restaurants.filter(restaurant => {
-				if (restaurant.name.toLowerCase() === name.toLowerCase()) {
-					restaurant.ratings = handleRating(restaurant.ratings, value);
-				}
-			});
+				Restaurants.filter(restaurant => {
+					if (restaurant.name.toLowerCase() === name.toLowerCase()) {
+						restaurant.ratings = handleRating(restaurant.ratings, value);
+					}
+				});
 
-			return res.send({ message: "Thanks for your ratings!" });
+				return res.send({ message: "Thanks for your ratings!" });
+			}
+			return res.status(400).send({ message: "name / value missing as a body parameter" });
 		}
-		return res.status(400).send({ message: "name / value missing as a body parameter" });
+		catch (e) {
+			return res.status(500).send({ message: e.message });
+		}
 	});
 
 };
